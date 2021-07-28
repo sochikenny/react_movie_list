be9a0d63ab48d6fdde6ed5599b2dd0f8e95bf7b5
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-07-28 13:03:24 -0400

    more functionality

--- movielist/src/components/Watchlist.js
+++ movielist/src/components/Watchlist.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { MovieCard } from "./MovieCard";
 
 export const Watchlist = () => {
-    return (
-        <div>
-            <h1>Watch List Page</h1>
+  const { watchlist } = useContext(GlobalContext);
+  return (
+    <div className="movie-page">
+      <div className="container">
+        <div className="header">
+          <h1 className="heading">My Watchlist</h1>
         </div>
-    )
-}
+
+        {watchlist.length > 0 ? (
+          <div className="movie-grid">
+            {watchlist.map((movie) => (
+              <MovieCard movie={movie} type="watchlist" />
+            ))}
+          </div>
+        ) : (
+          <h2 className="no-movies">No Movies in your watchlist. Add some</h2>
+        )}
+      </div>
+    </div>
+  );
+};
