fb762536a5db10d7b552623450fd11e4ac1b2315
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-07-28 09:52:25 -0400

    added router

---
+++ movielist/src/components/Add.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Add = () => {
+    return (
+       <div className="add-page">
+           <div className="container">
+               <div className="add-content">
+                   <div className="input-wrapper">
+                       <input type="text" placeholder="Search for Movies" />
+                   </div>
+               </div>
+           </div>
+       </div>
+    )
+}
