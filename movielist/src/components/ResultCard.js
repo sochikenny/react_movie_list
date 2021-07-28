ac77c58bcc33d3726d60352cf028ffca43fe1582
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-07-28 11:08:19 -0400

    functionality

---
+++ movielist/src/components/ResultCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const ResultCard = ({movie}) => {
+    return (
+        <div className="result-card">
+        <div className="poster-wrapper">
+            {movie.poster_path ? (
+                <img 
+                 src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                 alt={`${movie.title} Poster`}
+                />
+            ) : (
+                <div className="filler-poster"></div>
+            )}
+        </div>
+        
+    </div>
+    )
+       
+    
+}
