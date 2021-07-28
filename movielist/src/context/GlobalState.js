cf34ca4ada5aca0eed0358b44f3f220def0f1b11
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-07-28 12:33:20 -0400

    tweak watchlist state

--- movielist/src/context/GlobalState.js
+++ movielist/src/context/GlobalState.js
@@ -3,8 +3,12 @@ import AppReducer from "./AppReducer";
 
 //initial state
 const initialState = {
-  watchlist: [],
-  watched: [],
+  watchlist: localStorage.getItem("watchlist")
+    ? JSON.parse(localStorage.getItem("watchlist"))
+    : [],
+  watched: localStorage.getItem("watched")
+    ? JSON.parse(localStorage.getItem("watched"))
+    : [],
 };
 
 //create context
@@ -13,6 +17,12 @@ export const GlobalContext = createContext(initialState);
 //Provider Components
 export const GlobalProvider = (props) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  //using local storage with useEffect hook
+  useEffect(() => {
+    localStorage.setItem("watchlist", JSON.stringify(state.watchlist));
+    localStorage.setItem("watched", JSON.stringify(state.watched));
+  }, [state]);
 
   //actions
   const addMovieToWatchlist = (movie) => {
