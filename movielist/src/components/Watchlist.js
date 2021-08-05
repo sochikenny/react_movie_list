90a09eabae9ff81e374888d652398dadd94b6a89
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-08-05 10:21:18 -0400

    minor tweak

--- movielist/src/components/Watchlist.js
+++ movielist/src/components/Watchlist.js
@@ -18,7 +18,7 @@ export const Watchlist = () => {
         {watchlist.length > 0 ? (
           <div className="movie-grid">
             {watchlist.map((movie) => (
-              <MovieCard movie={movie} type="watchlist" />
+              <MovieCard movie={movie} type={"watchlist"} />
             ))}
           </div>
         ) : (
